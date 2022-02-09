c8bdd693d504f2988663aa342e720c2a2ad37c1f
Fernando Miranda <ingfermiranda@gmail.com>
2022-02-09 13:43:36 -0600

    se creo la clase Superhero

---
+++ app/src/main/java/com/nandomiranda/superheros/model/Superhero.kt
@@ -0,0 +1,8 @@
+package com.nandomiranda.superheros.model
+
+data class Superhero (val id: Int, val name: String,
+                      val power_int : Int, val power_stre : Int, val power_speed : Int, val power_durab : Int, val power_power : Int, val power_combat : Int,
+                      val bio_fullname: String, val bio_placeBirth: String, val bio_appearance: String, val bio_publisher: String,
+                      val appe_gender: String, val appe_race: String, val appe_height: String, val appe_weight: String, val appe_eyeColor: String, val appe_hairColor: String,
+                      val work_occupation: String, val work_base: String, val connec_affiliation: String, val connec_relatives: String,
+                      val image_Url: String)

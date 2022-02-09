c63cd2d936fdbde8de4b2da6fb1543ee3b2d64ed
Fernando Miranda <ingfermiranda@gmail.com>
2022-02-09 00:00:21 -0600

    creación de Fragments para lista de heroes y detalle de heroes

---
+++ app/src/main/java/com/nandomiranda/superheros/HeroListFragment.kt
@@ -0,0 +1,19 @@
+package com.nandomiranda.superheros
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HeroListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater , container: ViewGroup? ,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hero_list , container , false)
+    }
+
+}

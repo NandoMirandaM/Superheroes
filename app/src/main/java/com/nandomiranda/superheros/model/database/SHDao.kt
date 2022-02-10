c2c39cf836b4151fe3f5cca2bb52ba49b635e3f1
Fernando Miranda <ingfermiranda@gmail.com>
2022-02-10 12:25:54 -0600

    Se creo el dao y database para intentar guardar los datos directo en la database

---
+++ app/src/main/java/com/nandomiranda/superheros/model/database/SHDao.kt
@@ -0,0 +1,18 @@
+package com.nandomiranda.superheros.model.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.nandomiranda.superheros.model.superhero.Superhero
+
+@Dao
+interface SHDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(superheroList: MutableList<Superhero>)
+
+    @Query("Select * From superheros where id == :id")
+    fun getSuperheroDB(id: Int): MutableList<Superhero>
+
+
+}
---
+++ app/src/main/java/com/nandomiranda/superheros/model/database/SHDatabase.kt
@@ -0,0 +1,29 @@
+package com.nandomiranda.superheros.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.nandomiranda.superheros.model.superhero.Superhero
+import kotlinx.coroutines.InternalCoroutinesApi
+
+@Database(entities = [Superhero::class], version = 1)
+abstract class SHDatabase: RoomDatabase(){
+    abstract val SHdAO: SHDao
+}
+
+private lateinit var INSTANCE: SHDatabase
+
+@OptIn(InternalCoroutinesApi::class)
+fun getDatabase(context: Context) : SHDatabase{
+    kotlinx.coroutines.internal.synchronized(SHDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext ,
+                SHDatabase::class.java ,
+                "superhero_db"
+            ).build()
+        }
+        return INSTANCE
+    }
+}
